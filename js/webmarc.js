b9be266f93e1bf842296b7fd386ef7b1cc1d717b
Sergey Veselovskiy <s.veselovskiy@estp.ru>
2016-09-13 18:29:55 +0300

    Весь функционал перенесен в JS.

--- js/webmarc.js
+++ js/webmarc.js
@@ -1,42 +1,76 @@
-/**
- * Created by nimda on 10.09.2016.
- */
 "use strict";
 
-const SCREEN_H = 25;
 const SCREEN_W = 80;
+const SCREEN_H = 25;
 
 $(document).ready(function(){
 
-    // Размер экрана
-    var window_w = $(window).width();
-    var window_h = $(window).height();
-    console.log("window: ", window_w, "x", window_h);
-
-    // Размер блока scr
-    $(".scr").css("width", window_w + "px");
-    $(".scr").css("height", window_h + "px");
-    var scr_w = $(".scr").width();
-    var scr_h = $(".scr").height();
-    console.log("scr: ", scr_w, "x", scr_h);
-
-    // Размер grid
-    var grid_w = $(".grid").width();
-    var grid_h = $(".grid").height();
-    console.log("grid: ", grid_w, "x", grid_h);
-
-    // Коэффициенты трансформации
-    var transf_w = scr_w / grid_w;
-    var transf_h = scr_h / grid_h;
-    console.log("transf: ", transf_w, "x", transf_h);
-
-    // Трансформация
-    $(".grid").css("display","block");
-    $(".grid").css("width",grid_w);
-    $(".grid").css("transform-origin", transf_w/2 + "px " + transf_h/2 + "px");
-    $(".grid").css("transform", "scale("+transf_w+", "+transf_h+")");
+    // Инициализируем рабочую область
+    var ws = webcurses.initscr();
 
     // Выводим информацию
-    $(".grid").html("<span class='red'>Проверка</span>");
+    ws.html("<span class='info'>Проверка вывода текста в подготовленную рабочую область</span>");
+
 
 });
+
+var webcurses = {
+
+    initscr : function() {
+
+        if (! $(".container").length) {
+            $("body").append("<div class='container'></div>");
+        }
+        var container = $(".container");
+
+        container.append(this.buildWorkspace(SCREEN_W, SCREEN_H));
+
+        var ws = $(".workspace");
+
+        // Размер контейнера
+        var container_w = container.width();
+        var container_h = container.height();
+        //console.log("container: ", container_w, "x", container_h);
+
+        // Размер рабочей области
+        var ws_w = ws.width();
+        var ws_h = ws.height();
+        //console.log("workspace: ", ws_w, "x", ws_h);
+
+        // Коэффициенты трансформации
+        var transf_w = container_w / ws_w;
+        var transf_h = container_h / ws_h;
+        //console.log("transf: ", transf_w, "x", transf_h);
+
+        // Трансформация
+        ws.css("display", "block");
+        //ws.css("width", container_w);
+        ws.css("transform-origin", transf_w/2 + "px " + transf_h/2 + "px");
+        ws.css("transform", "scale("+transf_w+", "+transf_h+")");
+
+        // Очистка рабочей области
+        ws.html("");
+
+        return ws;
+    },
+
+    buildWorkspace : function(width, height) {
+        var grid = new Array();
+        for (var i=1; i<=height; i++) {
+            for (var j=1; j<=width; j++) {
+                if (
+                    i == 1 && j == 1
+                    || i == 1 && (j % 10 == 0)
+                    || (i % 5 == 0) && j == 1
+                    || (i % 5 == 0) && (j % 10 == 0)
+                ) {
+                    grid.push("⊹");
+                } else {
+                    grid.push("&nbsp;");
+                }
+            }
+            grid.push("<br/>");
+        }
+        return "<div class='workspace'>" + grid.join('') + "</div>";
+    }
+};
