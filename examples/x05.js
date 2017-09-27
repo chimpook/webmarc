7a992d95478694430ebcc2b613eadb1418e3a348
Sergey Veselovskiy <s.veselovskiy@estp.ru>
2017-09-27 18:20:50 +0300

    Переделываем реализацию примера.

--- examples/x05.js
+++ examples/x05.js
@@ -8,49 +8,10 @@ $(document).ready(function () {
 
     x.initscr($("#container"));
 
-    x.printw("Обычная строка...");
-
-    x.attron(x.A_BOLD);
-    x.printw("Жирная строка...");
-    x.attroff(x.A_BOLD);
-
-    x.printw("Вернулись к обычной строке...");
-
-    x.attron(x.A_BOLD | x.A_UNDERLINE);
-    x.printw("Жирная подчеркнутая строка...");
-    x.attroff(x.A_BOLD);
-
-    x.printw("Подчеркнутая строка...");
-
-    x.attron(x.A_REVERSE);
-
-    x.printw("Инверсия...");
-
-    x.attron(x.A_BLINK);
-
-    x.printw("Мигающая инверсия...");
-
-    x.attron(x.A_INVIS);
-    x.printw("Скрытый текст...");
-
-    x.attrset(x.A_NORMAL);
-
-    x.printw("Вернулись к обычной строке...");
-
-    x.init_pair(1, x.COLOR_LIGHTYELLOW, x.COLOR_DARKRED);
-    x.init_pair(2, x.COLOR_LIGHTCYAN, x.COLOR_DARKMAGENTA);
-
-    x.chgat(10, x.A_BOLD, 1);
-
-    x.printw("Цветная строка, но не вся...");
-
-    x.chgat(-1, x.A_BOLD, 2);
-
-    x.printw("Цветная строка, но не вся...");
-
-    x.mvchgat(1,10,5,x.A_BLINK,2);
-
-    x.printw("Ok.......");
+    /**
+     * TODO: 1)Загрузка текста исходного файла *.c, 2) выделение жирным шрифтом комментариев, 3) вывод постраничный
+     *
+     */
 
     x.refresh();
 });
