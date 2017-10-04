12ad0dccf5b31153b4cec0d3180e783d739ac6e8
Sergey Veselovskiy <s.veselovskiy@estp.ru>
2017-10-04 17:12:00 +0300

    Добавлен функционал Windows. Требуется полный рефакторинг.

--- examples/x07.c
+++ examples/x07.c
@@ -9,16 +9,17 @@ int main(int argc, char *argv[])
     int startx, starty, width, height;
     int ch;
 
-    initscr();
-    cbreak();
+    initscr();                          /* Start curses mode 		*/
+    cbreak();                           /* Line buffering disabled, Pass on
+             					         * everty thing to me 		*/
 
-    keypad(stdscr, TRUE);
+    keypad(stdscr, TRUE);               /* I need that nifty F1 	*/
 
     height = 3;
     width = 10;
 
-    starty = (LINES - height) / 2;
-    startx = (COLS - width) / 2;
+    starty = (LINES - height) / 2;      /* Calculating for a center placement */
+    startx = (COLS - width) / 2;        /* of the window		    */
 
     printw("Press F1 to exit");
     refresh();
@@ -50,7 +51,7 @@ int main(int argc, char *argv[])
                 break;
         }
     }
-    endwin();
+    endwin();                           /* End curses mode		    */
     return 0;
 }
 
@@ -59,16 +60,33 @@ WINDOW *create_newwin(int height, int width, int starty, int startx)
     WINDOW *local_win;
 
     local_win = newwin(height, width, starty, startx);
-    box(local_win, 0, 0);
+    box(local_win, 0, 0);               /* 0, 0 gives default characters
+                                         * for the vertical and horizontal
+                                         * lines			        */
 
-    wrefresh(local_win);
+    wrefresh(local_win);                /* Show that box 		    */
 
     return local_win;
 }
 
 void destroy_win(WINDOW *local_win)
 {
+    /* box(local_win, ' ', ' '); : This won't produce the desired
+	 * result of erasing the window. It will leave it's four corners
+	 * and so an ugly remnant of window.
+	 */
     wborder(local_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+    /* The parameters taken are
+     * 1. win: the window on which to operate
+     * 2. ls: character to be used for the left side of the window
+     * 3. rs: character to be used for the right side of the window
+     * 4. ts: character to be used for the top side of the window
+     * 5. bs: character to be used for the bottom side of the window
+     * 6. tl: character to be used for the top left corner of the window
+     * 7. tr: character to be used for the top right corner of the window
+     * 8. bl: character to be used for the bottom left corner of the window
+     * 9. br: character to be used for the bottom right corner of the window
+     */
     wrefresh(local_win);
     delwin(local_win);
 }
---
+++ examples/x07.js
@@ -0,0 +1,21 @@
+/**
+ * Example 7.
+ * Window Border example
+ */
+
+;"use strict";
+
+var app = {
+
+    wc: new Webcurses('Example 7. Window Border example'),
+
+    start: function () {
+        var self = this;
+        self.wc.initscr($("#container"));
+        self.wc.refresh();
+    }
+};
+
+$(document).ready(function () {
+    app.start();
+});
