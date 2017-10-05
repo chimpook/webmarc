6263968797b08a67b46cf4d3787eb4f585ed92a2
Sergey Veselovskiy <s.veselovskiy@estp.ru>
2017-10-05 17:37:14 +0300

    Добавлены методы перемещения и удаления окон.

--- examples/x07-1.js
+++ examples/x07-1.js
@@ -20,9 +20,14 @@ var app = {
 
         // Создаем и наполняем тестовое окно
         self.x.newwin('test', 12, 12, 5, 4);
-        self.x.wfillw(1, '@', self.x.COLOR_DARKBLUE, self.x.COLOR_CYAN);
-        self.x.wprintw(1, 'test');
+        self.x.wfillw('test', '@', self.x.COLOR_DARKBLUE, self.x.COLOR_CYAN);
+        self.x.wprintw('test', 'test');
 
+        self.x.newwin('temp', 10, 10, 10, 40);
+        self.x.wfillw('temp', '@', self.x.COLOR_DARKGRAY, self.x.COLOR_LIGHTGREEN);
+        self.x.wprintw('temp', 'temp');
+
+        //self.x.delwin('temp');
         self.x.refresh();
     }
 };
