1e0240e20c0933434f4adcd5e92093db3f662342
Sergey Veselovskiy <s.veselovskiy@estp.ru>
2017-10-05 14:39:26 +0300

    Добавлен метод проверки инициализации экрана.

--- examples/x07-1.js
+++ examples/x07-1.js
@@ -7,12 +7,23 @@
 
 var app = {
 
-    wc: new Webcurses('Example 7. Window Border example'),
+    x: new Webcurses('Example 7. Window Border example'),
 
     start: function () {
         var self = this;
-        self.wc.initscr($("#container"));
-        //self.wc.refresh();
+
+        self.x.initscr($("#container"));
+
+        // Наполняем стандартное окно
+        self.x.fillw('@', self.x.COLOR_DARKRED, self.x.COLOR_LIGHTYELLOW);
+        self.x.printw('stdscr');
+
+        // Создаем и наполняем тестовое окно
+        self.x.newwin('test', 12, 12, 5, 4);
+        self.x.wfillw(1, '@', self.x.COLOR_DARKBLUE, self.x.COLOR_CYAN);
+        self.x.wprintw(1, 'test');
+
+        self.x.refresh();
     }
 };
 
